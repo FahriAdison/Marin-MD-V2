92903138641598f8e29b4aaecfa98fc57987c90d
Yuta Togashi <92633211+FahriAdison@users.noreply.github.com>
2022-04-01 22:17:21 +0700

    Marin-MD-V2

--- plugins/play.js
+++ plugins/play.js
@@ -1,47 +1,33 @@
+// By Johannes 
+// Mau copas? silahkan Kasih Nama saya 🗿
+// Help BY YUTA
+
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
-const { servers, yta, ytv } = require('../lib/y2mate')
+const { youtubeSearch } = require('@bochilteam/scraper')
 let fs = require('fs')
-let yts = require('yt-search')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} california`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let yt2 = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
-    }
-  }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-let anu =  `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-*link sumber:* 
-${vid.url}
+  if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+  let vid = (await youtubeSearch(text)).video[0]
+  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+  const url = 'https://www.youtube.com/watch?v=' + videoId
+  let anu = `
+[ YOUTUBE PLAY ]
 
+📌 *Title:* ${title}
+🔗 *Url:* ${url}
+📩 *Description:* ${description}
+⏲️ *Published:* ${publishedTime}
+⌚ *Duration:* ${durationH}
+👁️ *Views:* ${viewH}
 `
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
            hydratedContentText: anu,
            locationMessage: { 
-           jpegThumbnail: await (await fetch(thumb)).buffer() }, 
+           jpegThumbnail: await (await fetch(thumbnail)).buffer() }, 
            hydratedFooterText: `jika video tidak sesuai
 silahkan ketik .yta link YouTube untuk mengunduh audio
 dan ketik .ytv link YouTube Untuk Mengunduh Video`,
@@ -54,22 +40,22 @@ dan ketik .ytv link YouTube Untuk Mengunduh Video`,
            },
                {
              quickReplyButton: {
-               displayText: 'video 360p',
-               id: `.ytmp4 ${vid.url}`,
-             }
-
-            },
-               {
-             quickReplyButton: {
-               displayText: 'video 720p',
-               id: `.ytv720 ${vid.url}`,
+               displayText: 'video ',
+               id: `${usedPrefix}ytv ${url}`,
              }
 
             },
                {
              quickReplyButton: {
                displayText: 'Audio',
-               id: `.ytmp3 ${vid.url}`,
+               id: `${usedPrefix}yta ${url}`,
+             }
+
+            },
+               {
+             quickReplyButton: {
+               displayText: 'Youtube Search',
+               id: `${usedPrefix}yts ${url}`, 
              }
 
            }]
@@ -90,4 +76,3 @@ handler.command = /^(p|play)$/i
 handler.exp = 0
 
 module.exports = handler
-
