598108482bed090651c9951f5593bbd794b8b4a8
Yuta Togashi <92633211+FahriAdison@users.noreply.github.com>
2022-04-06 09:01:58 +0700

    Marin-MD-V2

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,47 +1,47 @@
-const { sticker1, sticker5 } = require('../lib/sticker')
+const { sticker } = require('../lib/sticker.js')
+const { uploadFile } = require('../lib/uploadFile.js')
+const { uploadImage } = require('../lib/uploadImage.js')
+const { webp2png } = require('../lib/webp2mp4.js')
 
-let handler = async (m, { conn }) => {
-    let stiker = false
-    try {
-        let q = m.quoted ? m.quoted : m
-        let mime = (q.msg || q).mimetype || ''
-        if (/webp/.test(mime)) {
-            let img = await q.download()
-            if (!img) throw `reply sticker with command s`
-            stiker = await sticker5(img, false, packname, author)
-        } else if (/image/.test(mime)) {
-            let img = await q.download()
-            if (!img) throw `reply image with command s`
-            stiker = await sticker5(img, false, packname, author)
-        } else if (/video/.test(mime)) {
-            if ((q.msg || q).seconds > 11) return m.reply('maksimal 10 detik!')
-            let img = await q.download()
-            if (!img) throw `reply video with command s`
-            stiker = await sticker5(img, false, packname, author)
-        } else if (m.quoted.text) {
-            if (isUrl(m.quoted.text)) stiker = await sticker(false, m.quoted.text, packname, author)
-            else throw 'URL is not valid! end with jpg/gif/png'
-        }
-    } catch (e) {
-        throw e
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let stiker = false
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/webp|image|video/g.test(mime)) {
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      let img = await q.download?.()
+      if (!img) throw `balas gambar/video/stiker dengan perintah ${usedPrefix + command}`
+      let out
+      try {
+        if (/webp/g.test(mime)) out = await webp2png(img)
+        else if (/image/g.test(mime)) out = await uploadImage(img)
+        else if (/video/g.test(mime)) out = await uploadFile(img)
+        if (typeof out !== 'string') out = await uploadImage(img)
+        stiker = await sticker(false, out, global.packname, global.author)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) stiker = await sticker(img, false, global.packname, global.author)
+      }
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('URL tidak valid!')
     }
-    finally {
-        if (stiker) {
-            m.reply(stiker_wait)
-            await conn.sendFile(m.chat, stiker, '', '', m)
-        }
-        else {
-
-            throw 0
-        }
-    }
+  } catch (e) {
+    console.error(e)
+    if (!stiker) stiker = e
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    else throw 'Conversion failed'
+  }
 }
-handler.help = ['sticker']
+handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
-handler.command = /^(stiker|s|sticker)$/i
+handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
 module.exports = handler
 
 const isUrl = (text) => {
-    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
 }
